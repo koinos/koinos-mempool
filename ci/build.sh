85abb90cc22aaf82eef4c0d60f76b812338d1d57
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-04-16 21:42:27 -0700

    Add integration tests

--- ci/build.sh
+++ ci/build.sh
@@ -15,9 +15,14 @@ if [[ -z $BUILD_DOCKER ]]; then
       cmake --build . --config Debug --parallel 3 --target coverage
    fi
 else
+   TAG="$TRAVIS_BRANCH"
+   if [ "$TAG" = "master" ]; then
+      TAG="latest"
+   fi
+
    cp -R ~/.ccache ./.ccache
    docker build . -t koinos-mempool-ccache --target builder
-   docker build . -t koinos-mempool
+   docker build . -t koinos/koinos-mempool:$TAG
    docker run -td --name ccache koinos-mempool-ccache
    docker cp ccache:/koinos-mempool/.ccache ~/
 fi
--- ci/test.sh
+++ ci/test.sh
@@ -6,4 +6,18 @@ set -x
 if [ "$RUN_TYPE" = "test" ]; then
    cd $(dirname "$0")/../build/tests
    exec ctest -j3 --output-on-failure && ../libraries/vendor/mira/test/mira_test
+else
+   TAG="$TRAVIS_BRANCH"
+   if [ "$TAG" = "master" ]; then
+      TAG="latest"
+   fi
+
+   export MEMPOOL_TAG=$TAG
+
+   git clone https://github.com/koinos/koinos-integration-tests.git
+
+   cd koinos-integration-tests
+   go get ./...
+   cd tests
+   ./run.sh
 fi
