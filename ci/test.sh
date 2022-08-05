19eca6fac219a7fda4aabd0742c4190075573437
Steve Gerbino <steve@gerbino.co>
2022-08-05 15:06:56 -0400

    Add Valgrind to CI

--- ci/test.sh
+++ ci/test.sh
@@ -6,6 +6,9 @@ set -x
 if [ "$RUN_TYPE" = "test" ]; then
    cd $(dirname "$0")/../build/tests
    exec ctest -j3 --output-on-failure && ../libraries/vendor/mira/test/mira_test
+elif [ "$RUN_TYPE" = "coverage" ]; then
+   cd $(dirname "$0")/../build/tests
+   exec valgrind --error-exitcode=1 --leak-check=yes ./koinos_mempool_tests
 fi
 
 if ! [[ -z $BUILD_DOCKER ]]; then
